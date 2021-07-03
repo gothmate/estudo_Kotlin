53332dc255052328cd43981fd546a9fc982c92bd
Gothmate <gothmate@gmail.com>
2021-07-03 18:02:25 -0300

    mudança de funcionario e pessoa para abstract class

--- src/main/kotlin/one.digitalinnovation.digionebank/Pessoa.kt
+++ src/main/kotlin/one.digitalinnovation.digionebank/Pessoa.kt
@@ -1,16 +1,6 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa() {
-    var nome:String = "Gabriel Correia"
-    var cpf:String = "123.123.123.09"
-    private set
-
-    fun pessoaInfo() = "$nome CPF: $cpf"
-
-}
-
-
-fun main() {
-    val gabriel = Pessoa()
-    println(gabriel.pessoaInfo())
-}
+abstract class Pessoa(
+    val nome: String,
+    val cpf: String,
+)
